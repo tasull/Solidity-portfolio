cd8c3a92d251634aa0da8a73e628227a1f30b1f3
Taaniel Sülla <taaniel.sylla@gmail.com>
2021-12-10 23:36:11 +0200

    add content

---
+++ SimpleStorage.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SimpleStorage {
+
+    uint256 public favoriteNumber;
+    string helloMessage;
+
+    // This is a comment! hello
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+    
+    function retrieve() public view returns (uint256){
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+    function addHelloMsg(string memory _msg) public {
+        helloMessage = _msg;
+    }
+
+    function sayHello(string memory _name) public view returns (string memory) {
+        return string(abi.encodePacked(helloMessage, ", ", _name));
+    }
+
+}
